80a7d6c013a2fff8d30ccbf106479f3c672c6a99
DESTROYER <Rajaryan231207@gmail.com>
2025-07-17 07:33:03 +0530

    Update animation.py

--- Faded/modules/basic/animation.py
+++ Faded/modules/basic/animation.py
@@ -538,7 +538,7 @@ async def hak(client: Client, message: Message):
     await message.edit_text("Hacking complete!\nUploading file...")
     await asyncio.sleep(2)
     await message.edit_text(
-        "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12` 𝐅𝐎𝐑 𝐑𝐄𝐌𝐎𝐕𝐄 𝐌𝐄𝐄𝐓 𝐌𝐘 [ᴏᴡɴᴇʀ](https://t.me/CRY4DED_FR)"
+        "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12` ⚡𝐅𝐎𝐑 𝐑𝐄𝐌𝐎𝐕𝐄 𝐌𝐄𝐄𝐓 𝐌𝐘 🐉[ᴏᴡɴᴇʀ👻](https://t.me/CRY4DED_FR)"
     )
 
 
