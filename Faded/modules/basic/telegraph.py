9c6a0df80222d2ab92ae738734c13899894f6737
DESTROYER <Rajaryan231207@gmail.com>
2025-07-23 11:55:59 +0530

    Update telegraph.py

--- Faded/modules/basic/telegraph.py
+++ Faded/modules/basic/telegraph.py
@@ -1,86 +1,73 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from telegraph import Telegraph, exceptions, upload_file
 import os
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Faded import app
-import requests
 
 
-def upload_file(file_path):
-    url = "https://catbox.moe/user/api.php"
-    data = {"reqtype": "fileupload", "json": "true"}
-    files = {"fileToUpload": open(file_path, "rb")}
-    response = requests.post(url, data=data, files=files)
+from Zaid.modules.help import *
 
-    if response.status_code == 200:
-        return True, response.text.strip()
+telegraph = Telegraph()
+r = telegraph.create_account(short_name="telegram")
+auth_url = r["auth_url"]
+
+
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
     else:
-        return False, f"ᴇʀʀᴏʀ: {response.status_code} - {response.text}"
-
+        return None
 
 @Client.on_message(filters.command(["tg", "telegraph", "tm", "tgt"], ".") & filters.me)
-async def get_link_group(client, message):
+async def uptotelegraph(client: Client, message: Message):
+    tex = await message.edit_text("`Processing . . .`")
     if not message.reply_to_message:
-        return await message.reply_text(
-            "Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇᴅɪᴀ ᴛᴏ ᴜᴘʟᴏᴀᴅ ᴏɴ Tᴇʟᴇɢʀᴀᴘʜ"
+        await tex.edit(
+            "**Reply to an Image or text.**"
         )
-
-    media = message.reply_to_message
-    file_size = 0
-    if media.photo:
-        file_size = media.photo.file_size
-    elif media.video:
-        file_size = media.video.file_size
-    elif media.document:
-        file_size = media.document.file_size
-
-    if file_size > 200 * 1024 * 1024:
-        return await message.reply_text("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴇᴅɪᴀ ғɪʟᴇ ᴜɴᴅᴇʀ 200MB.")
-
-    try:
-        text = await message.reply("Pʀᴏᴄᴇssɪɴɢ...")
-
-        async def progress(current, total):
-            try:
-                await text.edit_text(f"📥 Dᴏᴡɴʟᴏᴀᴅɪɴɢ... {current * 100 / total:.1f}%")
-            except Exception:
-                pass
-
+        return
+    if message.reply_to_message.media:
+        if message.reply_to_message.sticker:
+            m_d = await convert_to_image(message, client)
+        else:
+            m_d = await message.reply_to_message.download()
         try:
-            local_path = await media.download(progress=progress)
-            await text.edit_text("📤 Uᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
-
-            success, upload_path = upload_file(local_path)
-
-            if success:
-                await text.edit_text(
-                    f"🌐 | [👉ʏᴏᴜʀ ʟɪɴᴋ ᴛᴀᴘ ʜᴇʀᴇ👈]({upload_path})",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton(
-                                    "𝖢ʀᴇᴀᴛᴇ ʙʏ Fᴀᴅᴇᴅ Usᴇʀ Bᴏᴛ 𝖳ᴀᴘ ᴛᴏ 𝖲ᴇᴇ",
-                                    url=upload_path,
-                                )
-                            ]
-                        ]
-                    ),
-                )
-            else:
-                await text.edit_text(
-                    f"ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ᴜᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ғɪʟᴇ\n{upload_path}"
-                )
-
-            try:
-                os.remove(local_path)
-            except Exception:
-                pass
-
-        except Exception as e:
-            await text.edit_text(f"❌ Fɪʟᴇ ᴜᴘʟᴏᴀᴅ ғᴀɪʟᴇᴅ\n\n<i>Rᴇᴀsᴏɴ: {e}</i>")
-            try:
-                os.remove(local_path)
-            except Exception:
-                pass
+            media_url = upload_file(m_d)
+        except exceptions.TelegraphException as exc:
+            await tex.edit(f"**ERROR:** `{exc}`")
+            os.remove(m_d)
             return
-    except Exception:
-        pass
+        U_done = (
+            f"**Uploaded on ** [Telegraph](https://telegra.ph/{media_url[0]})"
+        )
+        await tex.edit(U_done)
+        os.remove(m_d)
+    elif message.reply_to_message.text:
+        page_title = get_text(message) if get_text(message) else client.me.first_name
+        page_text = message.reply_to_message.text
+        page_text = page_text.replace("\n", "<br>")
+        try:
+            response = telegraph.create_page(page_title, html_content=page_text)
+        except exceptions.TelegraphException as exc:
+            await tex.edit(f"**ERROR:** `{exc}`")
+            return
+        wow_graph = f"**Uploaded as** [Telegraph](https://telegra.ph/{response['path']})"
+        await tex.edit(wow_graph)
+
+
+add_command_help(
+    "telegraph",
+    [
+        [
+            f"telegraph `or` .tg",
+            "To upload on telegraph.",
+        ],
+    ],
+)
